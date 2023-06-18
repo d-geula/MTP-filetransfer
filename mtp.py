263793be442193211c3f6396df29fd7a8abdedd4
Daniel Geula <d.geula@gmail.com>
2023-06-18 04:28:16 +0100

    Convert to class

--- mtp.py
+++ mtp.py
@@ -2,61 +2,76 @@ import subprocess
 from pathlib import Path
 
 
-def manage_storage(
-    device_name: str, storage_name: str, drive_letter: str, operation: str
-):
-    """
-    Mounts or unmounts the specified storage on the device.
-    """
+class MTPManager:
+    def __init__(self, mtpmount_path, device_name, storage_name, drive_letter: str):
+        self.mtpmount_path = Path(mtpmount_path)
+        self.process_name = self.mtpmount_path.name
+        self.device_name = device_name
+        self.storage_name = storage_name
+        self.drive_letter = drive_letter
 
-    mtpmount_dir = Path("tools/mtpmount-x64")
-    cmd = f'mtpmount {operation} {device_name} "{storage_name}" {drive_letter}'
-    subprocess.run(cmd, shell=True, cwd=mtpmount_dir)
+    def copy_files(self, src: list, dest, overwrite=False):
+        """
+        Copies one or more files or folders to the specified destination path.
 
+        Set `overwrite` to True if you want to overwrite existing files or folders without prompting.
+        """
 
-def copy_files(src: list, dest, overwrite=False):
-    """
-    Copies one or more files or folders to the specified destination path.
+        if self.is_running(self.process_name):
+            self.kill_process(self.process_name)
 
-    Set `overwrite` to True if you want to overwrite existing files or folders without prompting.
-    """
-    overwrite_flag = "/Y" if overwrite else ""
+        self.manage_storage("mount")
 
-    for src_path in src:
-        dest_item_path = Path(dest) / Path(src_path).name
+        overwrite_flag = "/Y" if overwrite else ""
 
-        if Path(src_path).is_file():
-            cmd = f'xcopy "{src_path}" "{dest}" {overwrite_flag}'
-            subprocess.run(cmd, shell=True, check=True)
+        for src_path in src:
+            dest_item_path = Path(dest) / Path(src_path).name
 
-        elif Path(src_path).is_dir():
-            cmd = f'xcopy "{src_path}" "{dest_item_path}" /E /I {overwrite_flag}'
-            subprocess.run(cmd, shell=True, check=True)
+            if Path(src_path).is_file():
+                cmd = f'xcopy "{src_path}" "{dest}" {overwrite_flag}'
+                self.run_cmd(cmd)
 
+            elif Path(src_path).is_dir():
+                cmd = f'xcopy "{src_path}" "{dest_item_path}" /E /I {overwrite_flag}'
+                self.run_cmd(cmd)
+
+            else:
+                raise ValueError(
+                    f"Invalid path: {src_path} is neither a file nor a folder."
+                )
+
+        self.manage_storage("unmount")
+
+    def manage_storage(self, operation: str):
+        """
+        Mounts or unmounts the specified storage on the device.
+        """
+
+        cmd = f'"{self.mtpmount_path}" {operation} "{self.device_name}" "{self.storage_name}" {self.drive_letter}'
+        self.run_cmd(cmd)
+
+    def run_cmd(self, cmd: str):
+        subprocess.run(cmd, shell=True, check=True)
+
+    def is_running(self, process_name: str) -> bool:
+        """
+        Check if the specified process is running.
+        """
+
+        # Check if the process is running
+        cmd = f'tasklist /fi "imagename eq {process_name}"'
+        process_check = subprocess.run(cmd, capture_output=True, text=True)
+
+        # Check if the process name is found in the tasklist output
+        if process_name.lower() in process_check.stdout.lower():
+            return True
         else:
-            raise ValueError(
-                f"Invalid path: {src_path} is neither a file nor a folder."
-            )
+            return False
 
+    def kill_process(self, process_name: str):
+        """
+        Terminates the specified process.
+        """
 
-def is_process_running(process_name: str) -> bool:
-    """
-    Checks if a process with the specified name is running.
-    """
-
-    # Check if the process is running
-    process_check_cmd = ["tasklist", "/fi", f"imagename eq {process_name}"]
-    process_check = subprocess.run(process_check_cmd, capture_output=True, text=True)
-
-    # Check if the process name is found in the tasklist output
-    if process_name.lower() in process_check.stdout.lower():
-        return True
-    else:
-        return False
-
-
-def terminate_process(process_name: str):
-    """
-    Terminates a process with the specified name.
-    """
-    subprocess.run(["taskkill", "/f", "/im", process_name])
+        cmd = f"taskkill /f /im {process_name}"
+        self.run_cmd(cmd)
