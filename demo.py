aac64b761b05dde922316a4ec90afd4e568f0fa1
Daniel Geula <d.geula@gmail.com>
2023-06-18 04:28:34 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,37 +1,18 @@
 from pathlib import Path
-import sys
-import mtp
-
-# Check the current platform
-if sys.platform != "win32":
-    current_platform = sys.platform
-    print(f"Unsupported platform: {current_platform}")
-    sys.exit(1)
-
-# Define file and destination paths
-files_to_copy = [
-    Path("files/test 1.jpg"),
-    Path("files/test2.jpg"),
-    Path("files/*.mp3"),
-    Path("files2"),
-]
+from mtp_class import MTPManager
 
 dest_path = Path("V:/")
+files_to_copy = [
+    Path("tests/files2"),  # Folder
+    Path("tests/files/test2.jpg"),  # File
+    Path("tests/files/test 1.jpg"),  # File with spaces in the name
+]
 
-# Check if the mtpmount process is running
+mtpmount_path = Path("tools/mtpmount-x64/mtpmount.exe")
+device_name = "M2007J20CG"
+storage_name = "Internal shared storage"
+drive_letter = "v"
 
-mtpmount_process = "mtpmount.exe"
-if mtp.is_process_running(mtpmount_process):
-    mtp.terminate_process(mtpmount_process)
+mtp = MTPManager(mtpmount_path, device_name, storage_name, drive_letter)
 
-# Mount the device
-mtp.manage_storage("MyDeviceName", "Internal shared storage", "v", "mount")
-
-# Copy the files
-mtp.copy_files(files_to_copy, dest_path)
-
-# Unmount the device
-mtp.manage_storage("MyDeviceName", "Internal shared storage", "v", "unmount")
-
-# Terminate the mtpmount process
-mtp.terminate_process(mtpmount_process)
+mtp.copy_files(files_to_copy, dest_path, overwrite=True)
