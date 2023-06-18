3f7c86084ec16c0a61578bda2d4e5ca88e8200a9
Daniel Geula <d.geula@gmail.com>
2023-06-18 21:49:32 +0100

    1 unmounted drive causing program to terminate (#2)

--- mtp.py
+++ mtp.py
@@ -10,10 +10,6 @@ class MTPManager:
         self.storage_name = storage_name
         self.drive_letter = drive_letter
 
-    def __del__(self):
-        self.close(self.process_name)
-        self.manage_storage("unmount")
-
     def copy_files(self, src: list, dest, overwrite=False):
         """
         Copies one or more files or folders to the specified destination path.
@@ -21,44 +17,63 @@ class MTPManager:
         Set `overwrite` to True if you want to overwrite existing files or folders without prompting.
         """
 
-        self.close(self.process_name)  # just in case the process is still running
-        self.manage_storage("mount")
+        self.kill_process(
+            self.process_name
+        )  # just in case the process is still running
 
-        overwrite_flag = "/Y" if overwrite else ""
+        try:
+            self.manage_storage("mount")
+            overwrite_flag = "/Y" if overwrite else ""
 
-        dest = Path(dest).resolve()
-        for src_path in src:
-            src_path = Path(src_path).resolve()
-            dest_item_path = dest / src_path.name
+            dest = Path(dest).resolve()
+            for src_path in src:
+                src_path = Path(src_path).resolve()
+                dest_item_path = dest / src_path.name
 
-            if src_path.is_file():
-                cmd = f'xcopy "{src_path}" "{dest}" {overwrite_flag}'
-                self.run_cmd(cmd)
+                if src_path.is_file():
+                    self.run_cmd(f'xcopy "{src_path}" "{dest}" {overwrite_flag}')
 
-            elif src_path.is_dir():
-                cmd = f'xcopy "{src_path}" "{dest_item_path}" /E /I {overwrite_flag}'
-                self.run_cmd(cmd)
+                elif src_path.is_dir():
+                    self.run_cmd(
+                        f'xcopy "{src_path}" "{dest_item_path}" /E /I {overwrite_flag}'
+                    )
 
-            else:
-                raise ValueError(
-                    f"Invalid path: {src_path} is neither a file nor a folder."
-                )
+                else:
+                    raise ValueError(
+                        f"Invalid path: {src_path} is neither a file nor a folder."
+                    )
 
-        self.manage_storage("unmount")
+            unmount = self.manage_storage("unmount")
+            if unmount.returncode == 0:
+                self.kill_process(self.process_name)
+
+        except Exception as e:
+            print(e)
+            self.kill_process(self.process_name)
 
     def manage_storage(self, operation: str):
         """
         Mounts or unmounts the specified storage on the device.
         """
-        cmd = f'"{self.mtpmount_path}" {operation} "{self.device_name}" "{self.storage_name}" {self.drive_letter}'
-        self.run_cmd(cmd)
 
-    def close(self, process_name):
+        return self.run_cmd(
+            f'"{self.mtpmount_path}" {operation} "{self.device_name}" "{self.storage_name}" {self.drive_letter}'
+        )
+
+    def kill_process(self, process_name):
         """
         Terminates the specified process if it is running.
         """
-        cmd = f"taskkill /f /im {process_name}"
-        self.run_cmd(cmd)
 
-    def run_cmd(self, cmd: str):
-        subprocess.run(cmd, shell=True, check=True)
+        # Check if the process is running
+        cmd = f'tasklist /fi "imagename eq {process_name}"'
+        process_check = subprocess.run(cmd, capture_output=True, text=True)
+
+        # Check if the process name is found in the tasklist output
+        if process_name.lower() in process_check.stdout.lower():
+            # Terminate the process
+            self.run_cmd(f"taskkill /f /im {process_name}")
+
+    def run_cmd(self, cmd):
+        results = subprocess.run(cmd, shell=True, check=True)
+        return results
