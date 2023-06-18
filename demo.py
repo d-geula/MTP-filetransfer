1a45aad59e4cc0a096b633638dec2aa13dab3129
Daniel Geula <d.geula@gmail.com>
2023-06-18 04:26:42 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -10,27 +10,28 @@ if sys.platform != "win32":
 
 # Define file and destination paths
 files_to_copy = [
-    Path('files/test 1.jpg'),
-    Path('files/test2.jpg'),
-    Path('files/*.mp3')
+    Path("files/test 1.jpg"),
+    Path("files/test2.jpg"),
+    Path("files/*.mp3"),
+    Path("files2"),
 ]
 
-dest_path = Path('V:/')
+dest_path = Path("V:/")
 
 # Check if the mtpmount process is running
 
-mtpmount_process = 'mtpmount.exe'
+mtpmount_process = "mtpmount.exe"
 if mtp.is_process_running(mtpmount_process):
     mtp.terminate_process(mtpmount_process)
 
 # Mount the device
-mtp.manage_storage('MyDeviceName', 'Internal shared storage', 'v', 'mount')
+mtp.manage_storage("MyDeviceName", "Internal shared storage", "v", "mount")
 
 # Copy the files
 mtp.copy_files(files_to_copy, dest_path)
 
 # Unmount the device
-mtp.manage_storage('MyDeviceName', 'Internal shared storage', 'v', 'unmount')
+mtp.manage_storage("MyDeviceName", "Internal shared storage", "v", "unmount")
 
 # Terminate the mtpmount process
 mtp.terminate_process(mtpmount_process)
