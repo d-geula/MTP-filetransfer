43b77d7a79fbcf21577effea247a991ab3034fa6
Daniel Geula <d.geula@gmail.com>
2023-06-18 04:27:01 +0100

    Update mtp.py

--- mtp.py
+++ mtp.py
@@ -2,54 +2,50 @@ import subprocess
 from pathlib import Path
 
 
-def manage_storage(device_name, storage_name, drive_letter, operation):
+def manage_storage(
+    device_name: str, storage_name: str, drive_letter: str, operation: str
+):
     """
     Mounts or unmounts the specified storage on the device.
-
-    Args:
-        device_name (str): The name of the device.
-        storage_name (str): The name of the storage to be managed.
-        drive_letter (str): The drive letter to assign to the mounted storage.
-        operation (str): The operation to perform. Possible values: "mount" or "unmount".
     """
 
     mtpmount_dir = Path("tools/mtpmount-x64")
-    mtpmount_cmd = f'mtpmount {operation} {device_name} "{storage_name}" {drive_letter}'
-    subprocess.run(mtpmount_cmd, shell=True, cwd=mtpmount_dir)
+    cmd = f'mtpmount {operation} {device_name} "{storage_name}" {drive_letter}'
+    subprocess.run(cmd, shell=True, cwd=mtpmount_dir)
 
 
-def copy_files(file_paths, destination_path):
+def copy_files(src: list, dest, overwrite=False):
     """
-    Copies one or more files to the specified destination path. Either full path(s) or wildcard(s).
-    
-    Args:
-        file_paths (str or list): The path(s) of the file(s) to be copied.
-            Can be a single file path as a string or a list of file paths. Wildcards are supported.
+    Copies one or more files or folders to the specified destination path.
 
-        destination_path (str): The destination path where the file(s) will be copied to.
+    Set `overwrite` to True if you want to overwrite existing files or folders without prompting.
     """
+    overwrite_flag = "/Y" if overwrite else ""
 
-    if isinstance(file_paths, str):
-        file_paths = [file_paths]  # Convert a single path to a list with one element
+    for src_path in src:
+        dest_item_path = Path(dest) / Path(src_path).name
 
-    for file_path in file_paths:
-        command = ["copy", file_path, destination_path]
-        subprocess.run(command, shell=True)
+        if Path(src_path).is_file():
+            cmd = f'xcopy "{src_path}" "{dest}" {overwrite_flag}'
+            subprocess.run(cmd, shell=True, check=True)
+
+        elif Path(src_path).is_dir():
+            cmd = f'xcopy "{src_path}" "{dest_item_path}" /E /I {overwrite_flag}'
+            subprocess.run(cmd, shell=True, check=True)
+
+        else:
+            raise ValueError(
+                f"Invalid path: {src_path} is neither a file nor a folder."
+            )
 
 
-def is_process_running(process_name):
+def is_process_running(process_name: str) -> bool:
     """
     Checks if a process with the specified name is running.
-
-    Args:
-        process_name (str): The name of the process to check.
-
-    Returns:
-        bool: True if the process is running, False otherwise.
     """
 
     # Check if the process is running
-    process_check_cmd = ['tasklist', '/fi', f'imagename eq {process_name}']
+    process_check_cmd = ["tasklist", "/fi", f"imagename eq {process_name}"]
     process_check = subprocess.run(process_check_cmd, capture_output=True, text=True)
 
     # Check if the process name is found in the tasklist output
@@ -57,13 +53,10 @@ def is_process_running(process_name):
         return True
     else:
         return False
-    
 
-def terminate_process(process_name):
+
+def terminate_process(process_name: str):
     """
     Terminates a process with the specified name.
-
-    Args:
-        process_name (str): The name of the process to terminate.
     """
-    subprocess.run(['taskkill', '/f', '/im', process_name])
+    subprocess.run(["taskkill", "/f", "/im", process_name])
